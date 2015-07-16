8f30013bcd97067b24edb600206035574c164f98
Jacob Penner <jkpenner91@gmail.com>
2015-07-16 15:49:54 -0700

    Comment RPGStatType

--- Assets/Scripts/RPGSystems/RPGStatType.cs
+++ Assets/Scripts/RPGSystems/RPGStatType.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Used to list off all stats that can be used
+/// within the RPGStatCollection class
+/// </summary>
 public enum RPGStatType {
     None = 0,
+
     Health = 1,
     Mana = 2,
 
