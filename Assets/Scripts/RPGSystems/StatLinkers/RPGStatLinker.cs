52ee281d734aa2a50b53b101e341f9cdb6fa2b81
Jacob Penner <jkpenner91@gmail.com>
2015-07-16 15:54:29 -0700

    Comment RPGStatLinker

--- Assets/Scripts/RPGSystems/StatLinkers/RPGStatLinker.cs
+++ Assets/Scripts/RPGSystems/StatLinkers/RPGStatLinker.cs
@@ -2,26 +2,41 @@
 using System;
 using System.Collections;
 
+/// <summary>
+/// The base class used by all stat linkers
+/// </summary>
 public abstract class RPGStatLinker : IStatValueChange {
-    private RPGStat _stat;
-
+    /// <summary>
+    /// Triggers when the Value of the linker changes
+    /// </summary>
     public event EventHandler OnValueChange;
 
-    public RPGStatLinker(RPGStat stat) {
-        _stat = stat;
+    /// <summary>
+    /// The RPGStat linked to by the stat linker
+    /// </summary>
+    public RPGStat LinkedStat { get; private set; }
 
-        IStatValueChange iValueChange = _stat as IStatValueChange;
+    /// <summary>
+    /// Gets the value of the stat linker
+    /// </summary>
+    public abstract int Value { get; }
+
+    /// <summary>
+    /// Basic Constructor. Listens to the Stat's OnValueChange
+    /// event if the stat implements IStatValueChange.
+    /// </summary>
+    public RPGStatLinker(RPGStat stat) {
+        LinkedStat = stat;
+
+        IStatValueChange iValueChange = LinkedStat as IStatValueChange;
         if (iValueChange != null) {
             iValueChange.OnValueChange += OnLinkedStatValueChange;
         }
     }
 
-    public RPGStat Stat {
-        get { return _stat; }
-    }
-
-    public abstract int Value { get; }
-
+    /// <summary>
+    /// Listens to the LinkedStat's OnValueChange event if able to
+    /// </summary>
     private void OnLinkedStatValueChange(object stat, EventArgs args) {
         if (OnValueChange != null) {
             OnValueChange(this, null);
--- Assets/Scripts/RPGSystems/StatLinkers/RPGStatLinkerBasic.cs
+++ Assets/Scripts/RPGSystems/StatLinkers/RPGStatLinkerBasic.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Basic implementation of a RPGStatLinker. Returns a percentage 
+/// of the Linked Stat
+/// </summary>
 public class RPGStatLinkerBasic : RPGStatLinker {
+    /// <summary>
+    /// The Ratio of the linked stat to use
+    /// </summary>
     private float _ratio;
 
+    /// <summary>
+    /// returns the ratio of the linked stat as the linker's value
+    /// </summary>
     public override int Value {
-        get { return (int)(Stat.StatValue * _ratio); }
+        get { return (int)(LinkedStat.StatValue * _ratio); }
     }
 
+    /// <summary>
+    /// Constructor that takes the linked stat and the ratio to use
+    /// </summary>
     public RPGStatLinkerBasic(RPGStat stat, float ratio) : base(stat) {
         _ratio = ratio;
     }
