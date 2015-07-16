00494fa2a669e443a0ad529f534659ca2253cf23
Jacob Penner <jkpenner91@gmail.com>
2015-07-16 15:39:13 -0700

    Comment RPGStatModifiable

--- Assets/Scripts/RPGSystems/StatTypes/RPGStatModifiable.cs
+++ Assets/Scripts/RPGSystems/StatTypes/RPGStatModifiable.cs
@@ -3,41 +3,75 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// A RPGStat Type that implements both IStatModifiable and IStatValueChange
+/// </summary>
 public class RPGStatModifiable : RPGStat, IStatModifiable, IStatValueChange {
+    /// <summary>
+    /// List of RPGStatModifiers applied to the stat
+    /// </summary>
     private List<RPGStatModifier> _statMods;
+
+    /// <summary>
+    /// Used by the StatModifierValue Property
+    /// </summary>
     private int _statModValue;
 
+    /// <summary>
+    /// Event that triggers when the stat value changes
+    /// </summary>
     public event System.EventHandler OnValueChange;
 
+    /// <summary>
+    /// The stat's total value including StatModifiers
+    /// </summary>
     public override int StatValue {
         get { return base.StatValue + StatModifierValue;}
     }
 
+    /// <summary>
+    /// The total value of the stat modifiers applied to the stat
+    /// </summary>
     public int StatModifierValue {
         get { return _statModValue; }
     }
 
+    /// <summary>
+    /// Basic Constructor
+    /// </summary>
     public RPGStatModifiable() {
         _statModValue = 0;
         _statMods = new List<RPGStatModifier>();
     }
 
+    /// <summary>
+    /// Triggers the OnValueChange Event
+    /// </summary>
     protected void TriggerValueChange() {
         if (OnValueChange != null) {
             OnValueChange(this, null);
         }
     }
 
+    /// <summary>
+    /// Adds Modifier to stat and listens to the mod's value change event
+    /// </summary>
     public void AddModifier(RPGStatModifier mod) {
         _statMods.Add(mod);
         mod.OnValueChange += OnModValueChange;
     }
 
+    /// <summary>
+    /// Removes modifier from stat and stops listening to value change event
+    /// </summary>
     public void RemoveModifier(RPGStatModifier mod) {
         _statMods.Add(mod);
         mod.OnValueChange -= OnModValueChange;
     }
 
+    /// <summary>
+    /// Removes all modifiers from the stat and stops listening to the value change event
+    /// </summary>
     public void ClearModifiers() {
         foreach (var mod in _statMods) {
             mod.OnValueChange -= OnModValueChange;
@@ -45,6 +79,9 @@ public class RPGStatModifiable : RPGStat, IStatModifiable, IStatValueChange {
         _statMods.Clear();
     }
 
+    /// <summary>
+    /// Updates the StatModifierValue based of the currently applied modifier values
+    /// </summary>
     public void UpdateModifiers() {
         _statModValue = 0;
 
@@ -69,6 +106,9 @@ public class RPGStatModifiable : RPGStat, IStatModifiable, IStatValueChange {
         TriggerValueChange();
     }
 
+    /// <summary>
+    /// Used to listen to the applied stat modifier OnValueChange events
+    /// </summary>
     public void OnModValueChange(object modifier, System.EventArgs args) {
         UpdateModifiers();
     }
