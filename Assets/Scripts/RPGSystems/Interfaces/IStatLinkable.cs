04711638ccc98abd2ce171997c56993e91b685c4
Jacob Penner <jkpenner91@gmail.com>
2015-07-16 15:45:47 -0700

    Comment RPGAttribute

--- Assets/Scripts/RPGSystems/Interfaces/IStatLinkable.cs
+++ Assets/Scripts/RPGSystems/Interfaces/IStatLinkable.cs
@@ -5,6 +5,7 @@ public interface IStatLinkable {
     int StatLinkerValue { get; }
 
     void AddLinker(RPGStatLinker linker);
+    void RemoveLinker(RPGStatLinker linker);
     void ClearLinkers();
     void UpdateLinkers();
 }
