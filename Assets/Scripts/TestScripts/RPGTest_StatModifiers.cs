2962fcd8b67b49c9e12dee7d9d1510c2f9f7f5c8
Jacob Penner <jkpenner91@gmail.com>
2015-07-16 15:14:43 -0700

    Updated The RPGTest_StatModifier

--- Assets/Scripts/TestScripts/RPGTest_StatModifiers.cs
+++ Assets/Scripts/TestScripts/RPGTest_StatModifiers.cs
@@ -2,32 +2,72 @@
 using System.Collections;
 
 public class RPGTest_StatModifiers : MonoBehaviour {
+    private RPGStatModifiable stat;
+
 	void Start () {
+        bool passed = true;
+        stat = new RPGStatModifiable();
+        
+        // Set the Starting base value
         int statTargetValue = 100;
-
-        RPGStatModifiable stat = new RPGStatModifiable();
         stat.StatBaseValue = statTargetValue;
+        
+        // Create instance of the modifiers with all values set to 0
+        RPGStatModifier[] mods = new RPGStatModifier[] {
+            new RPGStatModBaseAdd(0),
+            new RPGStatModBasePercent(0),
+            new RPGStatModTotalAdd(0),
+            new RPGStatModTotalPercent(0)
+        };
 
-        statTargetValue += 100;
-        stat.AddModifier(new RPGStatModBaseAdd(100));
-        stat.UpdateModifiers();
+        // Add lisener event to each modifier and add mod to stat
+        foreach (var mod in mods) {
+            //Debug.Log("Stat Listening to Mod: " + mod.GetType().ToString());
+            mod.OnValueChange += OnValueChange;
+            stat.AddModifier(mod);
+        }
 
-        Debug.Log(string.Format("Stat's value is {0}, target value is {1}", stat.StatValue, statTargetValue));
+        // Change the Base Add Mod Value
+        int baseAddValue = 100;
+        statTargetValue += baseAddValue;
+        mods[0].Value = baseAddValue;
 
-        statTargetValue += (int)(statTargetValue * 0.5f);
-        stat.AddModifier(new RPGStatModBasePercent(0.5f));
-        stat.UpdateModifiers();
+        // Check if the target value is the same as the stat's value
+        passed = passed && stat.StatValue == statTargetValue;
+        Debug.Log(string.Format("Check[{0}]: Stat Value {1}, Target Value {2}", passed, stat.StatValue, statTargetValue));
 
-        Debug.Log(string.Format("Stat's value is {0}, target value is {1}", stat.StatValue, statTargetValue));
+        // Change the Base Percent Mod Value
+        float basePercentValue = 0.5f;
+        statTargetValue += (int)(statTargetValue * basePercentValue);
+        mods[1].Value = basePercentValue;
 
-        statTargetValue += 100;
-        stat.AddModifier(new RPGStatModTotalAdd(100));
-        stat.UpdateModifiers();
+        // Check if the target value is the same as the stat's value
+        passed = passed && stat.StatValue == statTargetValue;
+        Debug.Log(string.Format("Check[{0}]: Stat Value {1}, Target Value {2}", passed, stat.StatValue, statTargetValue));
 
-        Debug.Log(string.Format("Stat's value is {0}, target value is {1}", stat.StatValue, statTargetValue));
+        // Change the Base Add Mod Value
+        int totalAddValue = 100;
+        statTargetValue += totalAddValue;
+        mods[2].Value = totalAddValue;
 
-        statTargetValue += (int)(statTargetValue * 0.5f);
-        stat.AddModifier(new RPGStatModTotalPercent(0.5f));
-        stat.UpdateModifiers();
+        // Check if the target value is the same as the stat's value
+        passed = passed && stat.StatValue == statTargetValue;
+        Debug.Log(string.Format("Check[{0}]: Stat Value {1}, Target Value {2}", passed, stat.StatValue, statTargetValue));
+
+        // Change the Base Percent Mod Value
+        float totalPercentValue = 0.5f;
+        statTargetValue += (int)(statTargetValue * totalPercentValue);
+        mods[3].Value = totalPercentValue;
+
+        // Check if the target value is the same as the stat's value
+        passed = passed && stat.StatValue == statTargetValue;
+        Debug.Log(string.Format("Check[{0}]: Stat Value {1}, Target Value {2}", passed, stat.StatValue, statTargetValue));
+
+        Debug.Log(string.Format("Stat Modifier Test: {0}", passed ? "Passed" : "Failed"));
 	}
+
+    private void OnValueChange(object mod, System.EventArgs args) {
+        //Debug.Log("On Value Changed Called For Modifier: " + mod.GetType().ToString());
+        stat.UpdateModifiers();
+    }
 }
