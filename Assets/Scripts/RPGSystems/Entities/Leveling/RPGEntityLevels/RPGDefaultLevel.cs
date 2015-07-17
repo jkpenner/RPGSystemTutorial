f26dcb0b169773329de36c0eeaf1fe2810e6f405
Jacob Penner <jkpenner91@gmail.com>
2015-07-17 15:42:47 -0700

    Add Default EntityLevel class

---
+++ Assets/Scripts/RPGSystems/Entities/Leveling/RPGEntityLevels/RPGDefaultLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Example Entity Level class the implements the GetExpRequiredForLevel method
+/// </summary>
+public class RPGDefaultLevel : RPGEntityLevel {
+    public override int GetExpRequiredForLevel(int level) {
+        return (int)(Mathf.Pow(level, 2f) * 100) + 100;
+    }
+}
